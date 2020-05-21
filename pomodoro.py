0c1df2ff978396a4efd5322c2a4b55aca2039230
spambi <dickjonesbb@protonmail.com>
2020-05-21 12:17:28 +1000

    Windows notifications with ToastNotifier

--- pomodoro.py
+++ pomodoro.py
@@ -1,25 +1,37 @@
 import time
+from win10toast import ToastNotifier
 
+toaster = ToastNotifier()
 
 def pomodoSwitch(lol) -> str:
     timeSwitch = {
-        0: pomodoroStart(25),
-        1: pomodoroRest(5),
-        2: pomodoroStop(0)
+        "0": pomodoroStart(25),
+        "1": pomodoroRest(5),
+        "2": pomodoroStop(0)
     }
     return switcher.get(lol, lambda: "[-] Invalid int %s" % timeSwitch)
 
 def pomodoroStart(time: float):
+    toaster.show_toast("Timer Start",
+                       "Timer has started for: {} minutes!".format(time),
+                       duration=3,
+                       threaded=True)
     print("[+] Started timer")
     basicTimer(time, 1.0)
 
 def pomodoroRest(time: float):
     print("[+] Started rest")
+    toaster.show_toast("Rest time!",
+                       "Resting for {} minutes!".format(int(time)),
+                       duration="3")
     basicTimer(time, 1.0)
 
 
 def pomodoroStop(time: float):
-    print("[+] Stopped tmier")
+    print("[+] Stopped timer")
+    toaster.show_toast("Timer Stopped!",
+                       "",
+                       duration="3")
     basicTimer(time, 1.0)
 
 def basicTimer(loltime: float, interval: float):
@@ -27,5 +39,8 @@ def basicTimer(loltime: float, interval: float):
         print(int(loltime))
         loltime -= interval
         time.sleep(interval)
+    toaster.show_toast("Timer is finished",
+                       duration=3)
 
-pomodoSwitch(int(input('Start: 0)))
+pomodoSwitch(int(input('Start: 0, Rest: 1, Stop: 2 ')))
+
